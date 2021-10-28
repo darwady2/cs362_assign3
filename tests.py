01302bf8059d72e7a8066bb1dc3703e08e3778d6
Dan Arwady <darwady2@gmail.com>
2021-10-27 20:23:07 -0700

    starting tests

--- tests.py
+++ tests.py
@@ -1,9 +1,18 @@
+import random
 import unittest
 from unittest import TestCase
 
 
 class Test(TestCase):
-    pass
+    def test_credit_card_validator_random_9_digit(self):
+        for i in range(0, 1000):
+            val = random.randint(10000000, 99999999)
+            mystery_func(val)
+
+    def test_credit_card_validator_random_10_digit(self):
+        pass
+
+
 
 if __name__ == '__main__':
     unittest.main()
